847be73c4e9393277d1c6c567cde468aaae45d93
Kalabasa <Kalabasa@users.noreply.github.com>
2023-12-27 21:11:59 +1100

    respond in html

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ use rouille::Response;
 
 fn main() {
     rouille::start_server("0.0.0.0:8080", move |request| {
-        Response::text("hello world")
+        Response::html("<!doctype html>
+            <title>My server</title>
+            <h1>Hello, world!</h1>
+        ")
     });
 }
