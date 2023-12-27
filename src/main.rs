f6f3de630cbd809bf4f62d0919c100e1b97cb3cc
Kalabasa <Kalabasa@users.noreply.github.com>
2023-12-27 21:39:49 +1100

    routes & pages

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,30 @@
-use rouille::Request;
 use rouille::Response;
+use rouille::router;
 
 fn main() {
     rouille::start_server("0.0.0.0:8080", move |request| {
-        Response::html("<!doctype html>
-            <title>My server</title>
-            <h1>Hello, world!</h1>
-        ")
+        let response = router!(request,
+            (GET) (/) => {
+                Response::html(r#"<!doctype html>
+                    <title>Tiny site</title>
+                    <h1>Hello, world!</h1>
+                    <p>Welcome to my tiny site!</p>
+                    <a href="/about">About</a>
+                "#)
+            },
+        
+            (GET) (/about) => {
+                Response::html(r#"<!doctype html>
+                    <title>About tiny site</title>
+                    <h1>About tiny site</h1>
+                    <p>This site is one file</p>
+                    <a href="/">Back to home</a>
+                "#)
+            },
+        
+            _ => Response::empty_404()
+        );
+
+        return response;
     });
 }
